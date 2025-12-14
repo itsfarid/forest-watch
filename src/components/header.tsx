fb0f6aa8a817fe1f97574f60fac853093956afb2
Farid | Iamjustzero <imjustzero@duck.com>
2025-12-14 20:49:42 +0700

    change to root only

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,16 @@
 import * as React from 'react'
 import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
-import { IconSeparator } from '@/components/ui/icons'
 import EnvCard from './cards/envcard'
 
 export async function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-white ">  
+    <header className="sticky top-0 z-50 flex items-center w-full h-16 px-4 border-b shrink-0 bg-white justify-between">  
+      <div className="flex items-center">
+        <Link href="/" rel="nofollow" className="mr-2 font-bold text-lg flex items-center gap-2">
+           🌲 Forest Watch
+        </Link>
+      </div>
       <EnvCard />
-      <Link href="/" rel="nofollow" className="mr-2 font-bold">
-        Forest Watch
-      </Link>
-      <IconSeparator />
-      <Link
-        href="/genui"
-        className={cn(buttonVariants({ variant: 'link' }), "mr-auto font-normal")}
-      >
-        <span className="hidden md:flex">Forest Watch AI</span>
-      </Link>
     </header>
   )
 }
